ddfae9b0da001b21b758126d436f6f1796d7fc8a
I <henrikl@tuta.io>
2021-07-20 10:39:29 +0200

    save before redo after tutorial

--- emailInward.py
+++ emailInward.py
@@ -1,3 +1,4 @@
+from email import message
 import imaplib
 import email
 from email.header import decode_header
@@ -7,22 +8,56 @@ import credentials
 
 imap = imaplib.IMAP4_SSL("imap.gmail.com")
 
-def getAndParseEmails():
-    status, messages = imap.select("INBOX")
-    #(retcode, messages) = imap.search(None, '(UNSEEN)')
-    n=0
-    for num in messages[0].split() :
-        n=n+1
-        typ, data = imap.fetch(num,'(RFC822)')
-        for response_part in data:
-            if isinstance(response_part, tuple):
-                original = email.message_from_bytes(response_part[1])
-                body = original.get_payload()
 
-                print (original['From'])
-                print (original['Subject'])
-                print("eno", body[0])
-                typ, data = imap.store(num,'+FLAGS','\\Seen')
+def emailToMap(emailMessage):
+    emailMap = {}
+
+
+    if emailMessage.is_multipart():
+        for payload in emailMessage.get_payload():
+            print (payload.get_payload())
+    else:
+        print (emailMessage.get_payload())
+
+    #emailMap["message"]
+    emailMap["from"] = emailMessage['From']
+    emailMap["subject"] = emailMessage['Subject']
+
+    #print("clips", emailMap["message"].as_string())
+    print("eno", emailMap)
+    #typ, data = imap.store(num,'+FLAGS','\\Seen')
+    return emailMap
+
+
+def getEmailMessage(emailIndex):
+    resCode, data = imap.fetch(emailIndex,'(RFC822)')
+    for response_part in data:
+        if isinstance(response_part, tuple):
+            return emailToMap(email.message_from_bytes(response_part[1]))
+
+
+
+
+
+def getEmailMessages(emailAccessors):   
+    messages = []
+
+    for emailAccessorsGroup in emailAccessors:
+        for emailIndex in emailAccessorsGroup.split() :
+            message = getEmailMessage(emailIndex)
+            messages.append(message)
+
+
+def getNewEmails():
+    inboxResCode, latestEmailAccessor = imap.select("INBOX")
+    #return imap.search(None, '(UNSEEN)')
+    return inboxResCode, latestEmailAccessor
+
+
+def getAndParseEmails():
+    resCode, newEmailAccessors = getNewEmails()
+    getEmailMessages(newEmailAccessors)
+
 
 def reloadMailbox():
     getAndParseEmails()
